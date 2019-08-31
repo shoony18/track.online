4d33ebac1440255adf1bcd2c3b73ace162b058d0
Karita Shuhei <shoony.1015@gmail.com>
2019-08-31 12:20:32 +0900

    Initial Commit

---
+++ track_online/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  track_online
+//
+//  Created by 刈田修平 on 2019/08/31.
+//  Copyright © 2019年 刈田修平. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
